345af9b8b99d0f61e6c955a05b8bf3080641b2a8
习惯成自然 <42534917+wzdhwy2@users.noreply.github.com>
2018-09-26 19:24:56 +0800

    9-26-1

--- linux/linux/3-4种符.sh
+++ linux/linux/3-4种符.sh
@@ -13,6 +13,9 @@
 ls -alh
 echo “查看当前目录详细情况！“
 
+
+
+#3.文件比较符
 [ -e ./shell.txt ]
 echo $?
 #[apps@test5153 linux]$ 0
@@ -22,9 +25,12 @@ ls -alh shell.txt
 [ $? -eq 0 ] && echo "shell.txt 验证了文件确实存在！" || echo "shell.txt 文件不存在！ "
 
 
+#4.字符比较运算符
 echo $USER
 #apps
 [ $USER = root  ] && echo "当前用户为root" || echo "当前用户为$USER"
 #当前用户为apps
-
+echo $USER
+[ $USER != root ] && echo "非root" || echo "是root"
+#非root
 
