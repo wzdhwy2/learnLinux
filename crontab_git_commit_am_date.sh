16c3a6f8cdc7f7317e48e955e6835cb6cb045c5e
习惯成自然 <42534917+wzdhwy2@users.noreply.github.com>
2018-10-17 14:30:01 +0800

    Wed Oct 17 14:30:01 CST 2018

--- crontab_git_commit_am_date.sh
+++ crontab_git_commit_am_date.sh
@@ -1,6 +1,5 @@
 #/bin/bash/
-#sh /usr/michael/learnlinux/crontab_git_commit_am_date.sh 
-#&>> crontab_git_commit_am_date.log
+#sh /usr/michael/learnlinux/crontab_git_commit_am_date.sh &>> crontab_git_commit_am_date.log——逻辑有问题
 #定时任务中，用来提交git仓库的脚本
 
 cd /usr/michael/learnlinux/
