2ff04ed87c522d6468515a23a170f03778c6fb6c
习惯成自然 <42534917+wzdhwy2@users.noreply.github.com>
2018-11-09 16:00:08 +0800

    Fri Nov  9 16:00:08 CST 2018

--- shell/11-8/dBname.sh
+++ shell/11-8/dBname.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#
+#取到MySQL服务器上所有库的库名，输出到本机此~/mysql_sh目录下
+
 DBname="DBname.txt"
+mkdir ~/mysql_sh
 
 mysql -h 10.200.53.2 -P 3306 -u sys_market_test -psys_market_test -e"
 show databases ;
 exit
-" >./$DBname
+" >~/mysql_sh/$DBname
 
 #sed '1d' $DBnme
 #sed -n '1p' $DBname
--- shell/11-8/table.sh
+++ shell/11-8/table.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
 
-sh ./dBname.sh
+#sh ~/mysql_sh/dBname.sh
 
 #取库名
 #——————————————————————————————————————————
 
 
-DBname="` cat DBname.txt `"
+DBname="` cat ~/mysql_sh/DBname.txt `"
 logdate="` date +%Y%m%d `"
-mkdir $logdate
+mkdir ~/mysql_sh/$logdate
 
 for DBnameS in $DBname
 do
@@ -18,10 +18,10 @@ SELECT TABLE_name,TABLE_rows,(DATA_LENGTH+INDEX_LENGTH) AS TOTAL_B
 FROM information_schema.TABLES 
 WHERE TABLE_SCHEMA='$DBnameS' ;
 exit
-" >./$logdate/$DBnameS
-sed -i '1d' ./$logdate/$DBnameS
+" >~/mysql_sh/$logdate/$DBnameS
+sed -i '1d' ~/mysql_sh/$logdate/$DBnameS
 done
 
-#取一天的情况，放到当天的文件夹里，每个库单独一个文件
+#取一天的情况，放到当天的文件夹里，每个库单独输出一个文件
 #——————————————————————————————————————————
 
